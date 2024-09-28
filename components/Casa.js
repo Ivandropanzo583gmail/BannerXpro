9211085c2eb4dcf98d93223efe3b78e2a6f7bf6c
Ivandro Panzo <ivandropanzo583@gmail.com>
2024-09-28 15:15:14 +0100

    Primeiro commit

--- components/Casa.js
+++ components/Casa.js
@@ -4,8 +4,8 @@ export default function Casa(){
             <div className="bg-zinc-800 md:flex block justify-center items-center md:h-screen h-full ">
 
                 <img src="./banner1.png" className="md:h-96 " />
-                <img src="./banner1.png" className="md:h-96 md:pl-10 max-md:mt-10" />
-                <img src="./banner1.png" className="md:h-96  md:pl-10 max-md:mt-10" />
+                <img src="./banner2.png" className="md:h-96 md:pl-10 max-md:mt-10" />
+                <img src="./banner3.png" className="md:h-96  md:pl-10 max-md:mt-10" />
               
 
             </div>
